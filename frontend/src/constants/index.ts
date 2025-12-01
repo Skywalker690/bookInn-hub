5738ade0d241c9edab5695c6e9d9de9213046ecb
Skywalker690 <sanjoksiby0@gmail.com>
2025-12-01 19:54:55 +0530

    fix: Remove fallback URL for REACT_APP_API_BASE_URL to enforce environment variable usage

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,6 +1,6 @@
 
 //export const REACT_APP_API_BASE_URL =  'http://localhost:8080';
-export const REACT_APP_API_BASE_URL = import.meta.env.VITE_REACT_APP_API_BASE_URL || 'http://localhost:8080';
+export const REACT_APP_API_BASE_URL = import.meta.env.VITE_REACT_APP_API_BASE_URL;
 
 export const ROOM_TYPES = [
   "Single",
